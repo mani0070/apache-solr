0b64599320d93acb8e88d6f548db58a242895b95
Manimaran Chandrasekaran <mani@dinventive.com>
2016-12-15 23:31:49 +0000

    apache solr choco package

---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,25 @@
+﻿# IMPORTANT: Before releasing this package, copy/paste the next 2 lines into PowerShell to remove all comments from this file:
+#   $f='c:\path\to\thisFile.ps1'
+#   gc $f | ? {$_ -notmatch "^\s*#"} | % {$_ -replace '(^.*?)\s*?[^``]#.*','$1'} | Out-File $f+".~" -en utf8; mv -fo $f+".~" $f
+
+## NOTE: In 80-90% of the cases (95% with licensed versions due to Package Synchronizer and other enhancements), you may 
+## not need this file due to AutoUninstaller. See https://chocolatey.org/docs/commands-uninstall
+
+## If this is an MSI, cleaning up comments is all you need.
+## If this is an exe, change installerType and silentArgs
+## Auto Uninstaller should be able to detect and handle registry uninstalls (if it is turned on, it is in preview for 0.9.9).
+## - https://chocolatey.org/docs/helpers-uninstall-chocolatey-package
+
+$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'apache-solr'
+$softwareName = 'apache-solr*' #part or all of the Display Name as you see it in Programs and Features. It should be enough to be unique
+
+
+## OTHER HELPERS
+## https://chocolatey.org/docs/helpers-reference
+Uninstall-ChocolateyZipPackage $packageName # Only necessary if you did not unpack to package directory - see https://chocolatey.org/docs/helpers-uninstall-chocolatey-zip-package
+#Uninstall-ChocolateyEnvironmentVariable # 0.9.10+ - https://chocolatey.org/docs/helpers-uninstall-chocolatey-environment-variable 
+#Uninstall-BinFile # Only needed if you used Install-BinFile - see https://chocolatey.org/docs/helpers-uninstall-bin-file
+## Remove any shortcuts you added
+
